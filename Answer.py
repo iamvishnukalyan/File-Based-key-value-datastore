41351ec7ba446420e3f7c33b5021efe7dfaec960
Vishnu Kalyan <vishnukalyan77@gmail.com>
2020-12-30 18:43:40 +0530

    Added answer.py file
    This file explains how to access the data using functions from the library file

---
+++ Answer.py
@@ -0,0 +1,17 @@
+
+#importing our library as lib
+import library as lib
+
+#creating a key-value pair using create()
+lib.create("age",21)
+
+#accessing the created key-value pair using read() function
+#only key must be passed and respective value will be obtained
+lib.read("age")
+
+#21 will be obtained as output
+
+#deleting a key-value pair using delete() function
+#only key must be passed and respective value will be deleted
+lib.delete("age")
+
