52a6ed52a741f67ef647fa4c73f8c51b61bdb254
vishnukalyan <vishnukalyan77@gmail.com>
2020-12-30 21:04:40 +0530

    updated library.py
    Implemented size constraints for key and value

--- library.py
+++ library.py
@@ -5,10 +5,15 @@ def create(key,value):
     if key in s:
         print("Key already exists")
     else:
-        if(key.isalpha()): #enters only if key is alphabetic
+        if(key.isalpha()):          #enters only if key is alphabetic
+            allowed_file_size = 1024*1024*1024          #Allowed file size os 1gb
+            val_size = 16*1024          #Allowed value size is 16kb
+            if len(s)<allowed_file_size and value<=val_size:
                 val=[value]
-                if len(key)<=32: #length of key must be <= 32chars
+                if len(key)<=32:     #length of key must be <= 32chars
                     s[key]=val
+            else:
+                print("Memory limit exceeded")
         else:
             print("Key name not correct")
 
@@ -16,7 +21,7 @@ def create(key,value):
 #read
 def read(key):
     if key in s: 
-        val = s[key]
+        val = str(key)+":"+ str(s[key])
         return val #returns respective value of the key
     else:
         print("Doesn't exists")
@@ -27,6 +32,7 @@ def read(key):
 def delete(key):
     if key in s:
         del s[key] #deletes the key
+        print(key,"<--- deleted successfully")
     else:
         print("key doesn't exists")
     
