f64bc2753b72f2804d4d34b558db5801ae118f26
vishnukalyan <vishnukalyan77@gmail.com>
2020-12-31 19:35:25 +0530

    Implemented timeout feature
    time to live property for a key-value pair is implemented

--- Answer.py
+++ Answer.py
@@ -7,11 +7,19 @@ lib.create("age",21)
 
 #accessing the created key-value pair using read() function
 #only key must be passed and respective value will be obtained
+#if the time-to-live hasn't expired
 lib.read("age")
 
 #21 will be obtained as output
 
 #deleting a key-value pair using delete() function
 #only key must be passed and respective value will be deleted
+#if the time-to-live hasn't expired
 lib.delete("age")
 
+#threading
+#threads can be used to access by passing functions to targets and arguments
+t.start()       #starts the thread
+t.sleep() 
+
+#many threads can be created in this way for accessing
--- library.py
+++ library.py
@@ -1,15 +1,23 @@
 s={} #dict to store data
+import time
+import thread
+from thread import*
 
 #create
-def create(key,value):
+def create(key,value,timeout=0):
     if key in s:
         print("Key already exists")
     else:
         if(key.isalpha()):          #enters only if key is alphabetic
+
             allowed_file_size = 1024*1024*1024          #Allowed file size os 1gb
             val_size = 16*1024          #Allowed value size is 16kb
+            
             if len(s)<allowed_file_size and value<=val_size:
-                val=[value]
+                if timeout==0:
+                    val=[value, timeout]
+                else:
+                    val = [value,time.time()+timeout]
                 if len(key)<=32:     #length of key must be <= 32chars
                     s[key]=val
             else:
@@ -20,9 +28,14 @@ def create(key,value):
             
 #read
 def read(key):
-    if key in s: 
-        val = str(key)+":"+ str(s[key])
-        return val #returns respective value of the key
+    if key in s:
+        t=s[key]
+        if t[1]!=0:
+            if time.time()<t[1]:    #checking ttl of the key
+                val = str(key)+":"+ str(t[0])
+                return val      #returns respective value of the key
+            else:
+                print("time-to-live is expired")
     else:
         print("Doesn't exists")
     
@@ -31,8 +44,16 @@ def read(key):
 #delete
 def delete(key):
     if key in s:
-        del s[key] #deletes the key
-        print(key,"<--- deleted successfully")
+        t=s[key]
+        if t[1]!=0:
+            if time.time()<t[1]:    #checking ttl of the key
+                del s[key]      #deletes the key
+                print(key,"<--- deleted successfully")
+            else:
+                print("time-to-live is expired")
+        else:
+            del s[key]      #deletes the key
+            print(key,"<--- deleted successfully")
     else:
         print("key doesn't exists")
     
